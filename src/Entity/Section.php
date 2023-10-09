fcb7d7e7f8b0c6f4ecc2dafde0213dc7e5d78ed1
Liantsiky <malalatsiky11@gmail.com>
2023-10-09 12:38:12 +0300

    Creation des classe pour le questionnaire

---
+++ src/Entity/Section.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SectionRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SectionRepository::class)]
+class Section
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sections')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?TestAptitude $test_aptitude = null;
+
+    #[ORM\Column(length: 200)]
+    private ?string $des_question = null;
+
+    #[ORM\Column]
+    private ?int $coef_section = null;
+
+    #[ORM\OneToMany(mappedBy: 'section', targetEntity: ReponseSection::class)]
+    private Collection $reponseSections;
+
+    public function __construct()
+    {
+        $this->reponseSections = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTestAptitude(): ?TestAptitude
+    {
+        return $this->test_aptitude;
+    }
+
+    public function setTestAptitude(?TestAptitude $test_aptitude): static
+    {
+        $this->test_aptitude = $test_aptitude;
+
+        return $this;
+    }
+
+    public function getDesQuestion(): ?string
+    {
+        return $this->des_question;
+    }
+
+    public function setDesQuestion(string $des_question): static
+    {
+        $this->des_question = $des_question;
+
+        return $this;
+    }
+
+    public function getCoefSection(): ?int
+    {
+        return $this->coef_section;
+    }
+
+    public function setCoefSection(int $coef_section): static
+    {
+        $this->coef_section = $coef_section;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ReponseSection>
+     */
+    public function getReponseSections(): Collection
+    {
+        return $this->reponseSections;
+    }
+
+    public function addReponseSection(ReponseSection $reponseSection): static
+    {
+        if (!$this->reponseSections->contains($reponseSection)) {
+            $this->reponseSections->add($reponseSection);
+            $reponseSection->setSection($this);
+        }
+
+        return $this;
+    }
+
+    public function removeReponseSection(ReponseSection $reponseSection): static
+    {
+        if ($this->reponseSections->removeElement($reponseSection)) {
+            // set the owning side to null (unless already changed)
+            if ($reponseSection->getSection() === $this) {
+                $reponseSection->setSection(null);
+            }
+        }
+
+        return $this;
+    }
+
+}
---
+++ src/Entity/TestAptitude.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TestAptitudeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TestAptitudeRepository::class)]
+class TestAptitude
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\OneToOne(inversedBy: 'testAptitude', cascade: ['persist', 'remove'])]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?CVRequirements $cv_requirements = null;
+
+    #[ORM\OneToMany(mappedBy: 'test_aptitude', targetEntity: Section::class)]
+    private Collection $sections;
+
+    public function __construct()
+    {
+        $this->sections = new ArrayCollection();
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCvRequirements(): ?CVRequirements
+    {
+        return $this->cv_requirements;
+    }
+
+    public function setCvRequirements(CVRequirements $cv_requirements): static
+    {
+        $this->cv_requirements = $cv_requirements;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Section>
+     */
+    public function getSections(): Collection
+    {
+        return $this->sections;
+    }
+
+    public function addSection(Section $section): static
+    {
+        if (!$this->sections->contains($section)) {
+            $this->sections->add($section);
+            $section->setTestAptitude($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSection(Section $section): static
+    {
+        if ($this->sections->removeElement($section)) {
+            // set the owning side to null (unless already changed)
+            if ($section->getTestAptitude() === $this) {
+                $section->setTestAptitude(null);
+            }
+        }
+
+        return $this;
+    }
+
+    
+}
