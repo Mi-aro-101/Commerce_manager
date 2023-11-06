e64c4346315afe3f4a24e47f15d1a6174fc11352
Mi-aro-101 <miaroramananadraitsiory@gmail.com>
2023-11-06 09:55:23 +0530

    Fix

--- migrations/Version20231106042357.php
+++ migrations/Version20231106042357.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231105053048 extends AbstractMigration
+final class Version20231106042357 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,13 +20,21 @@ final class Version20231105053048 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE employe ADD service_id INT NOT NULL');
+        $this->addSql('ALTER TABLE employe ADD statut INT NOT NULL');
         $this->addSql('ALTER TABLE employe ADD poste VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE employe ADD CONSTRAINT FK_F804D3B9ED5CA9E6 FOREIGN KEY (service_id) REFERENCES service (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('CREATE INDEX IDX_F804D3B9ED5CA9E6 ON employe (service_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE employe DROP CONSTRAINT FK_F804D3B9ED5CA9E6');
+        $this->addSql('DROP INDEX IDX_F804D3B9ED5CA9E6');
+        $this->addSql('ALTER TABLE employe DROP service_id');
+        $this->addSql('ALTER TABLE employe DROP statut');
         $this->addSql('ALTER TABLE employe DROP poste');
     }
 }
